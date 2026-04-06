9d0337dffae5b5a1536ebd8ba32dcea941d1e995
Hazardooo <hazdoo@outlook.com>
2026-04-06 17:48:51 +0300

    add new frontend files

---
+++ frontend/src/app/devices/new/page.tsx
@@ -0,0 +1,8 @@
+export default function NewDevicePage() {
+  return (
+    <div>
+      <h1>New Device</h1>
+      <form></form>
+    </div>
+  );
+}
---
+++ frontend/src/app/devices/page.tsx
@@ -0,0 +1,7 @@
+export default function DevicesPage() {
+  return (
+    <div>
+      <h1>Devices List</h1>
+    </div>
+  );
+}
